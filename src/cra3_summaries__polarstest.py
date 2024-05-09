aeebcde2e29d663a9d1a9f88c8f3fc812b510f98
Kevin Wong <kevinw26@gmail.com>
2024-05-08 21:50:16 -0400

    polars test transmittal groupby code

---
+++ src/cra3_summaries__polarstest.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 8 May 2024 20:01:40
+@author: Kevin.Wong
+"""
+import lzma
+import os
+import timeit
+from glob import glob
+from os import path
+
+import pandas as pd
+import polars as pl
+
+
+def polars():
+    trans = pl.concat(
+        (
+            pl.read_csv(lzma.open(i)) for i in
+            sorted(glob(path.join('out', 'trans', '*.csv.xz')))
+        ),
+        how='diagonal'
+    )
+    gb: pl.DataFrame = trans.group_by('activity_year').agg(
+        pl.concat_str(
+            [pl.col('agency_code'), pl.col('respondent_id')],
+            separator='-'
+        ).count().alias('respondent_ct'),
+        pl.col('id_rssd').count().alias('rssd_ct')
+    ).sort(by=['activity_year'])
+    os.makedirs(path.join('out', 'summary'), exist_ok=True)
+    gb.write_csv(path.join('out', 'summary', 'transmittal_counts.csv'))
+
+
+def pandas():
+    trans = pd.concat(
+        (
+            pd.read_csv(i) for i in
+            sorted(glob(path.join('out', 'trans', '*.csv.xz')))
+        ), axis=0
+    )
+    trans['_agcdrespid'] = (
+            trans['agency_code'].astype(str) + '-'
+            + trans['respondent_id'].astype(str)
+    )
+    gb = trans.groupby('activity_year').agg(
+        respondent_ct=('_agcdrespid', 'count'),
+        rssd_ct=('id_rssd', 'count')
+    ).sort_index()
+    os.makedirs(path.join('out', 'summary'), exist_ok=True)
+    gb.to_csv(path.join('out', 'summary', 'transmittal_counts.csv'),
+              index=True)
+
+
+if __name__ == '__main__':
+    '''
+    Apple M2, 16 GB RAM
+     - polars elapsed 3.8690 s (37 pc faster!)
+     - pandas elapsed 6.1175 s
+    '''
+    print('polars elapsed {}'.format(timeit.timeit(polars, number=50)))
+    print('pandas elapsed {}'.format(timeit.timeit(pandas, number=50)))
